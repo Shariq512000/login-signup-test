c95358310344f5b4c70df2830b2fc4126abccaf6
Shariq Siddiqui <99816206+Shariq512000@users.noreply.github.com>
2026-03-06 09:36:56 +0500

    init project

---
+++ script.js
@@ -0,0 +1,61 @@
+let localStrVal = localStorage.getItem("users") // null
+
+let users = []
+
+if(localStrVal){
+    users = JSON.parse(localStrVal)
+}
+
+function registerUser(){
+    let userName = document.getElementById("userName").value
+    let userEmail = document.getElementById("userEmail").value
+    let userPassword = document.getElementById("userPassword").value
+
+    for(let i = 0; i < users.length; i++){
+        if(users[i].email == userEmail){
+            alert("Already Register Account on this Email")
+            return;
+        }
+    }
+
+    let userObj = {
+        name: userName,
+        email: userEmail,
+        password: userPassword
+    }
+    users.push(userObj)
+
+    let convertUserToStr = JSON.stringify(users)
+
+    localStorage.setItem("users", convertUserToStr)
+}
+
+console.log(users)
+
+console.log(window.location.pathname)
+
+function loginUser(){
+    let userEmail = document.getElementById("userEmail").value;
+    let userPassword = document.getElementById("userPassword").value;
+
+    let currentUser = null;
+
+    for(let i = 0; i < users.length; i++){
+        if(users[i].email == userEmail){
+            currentUser = users[i];
+            break;
+        }
+    }
+
+    if(currentUser){
+
+        if(currentUser.password == userPassword){
+            // alert("User Logedin")
+        }else{
+            alert("Password did not matched")
+        }
+
+    }else{
+        alert("Account Not Register with this email")
+    }
+}
